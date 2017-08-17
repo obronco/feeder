ba9b29876714ae81cd52657f8dd5894a5a9edd28
Leonardo Horta <leonardo.horta@sparta.com.br>
2017-08-17 13:04:28 -0300

    cors enabled

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import datetime
 import json
 import os
 
-from bottle import route, run, template, request, static_file  #, hook, response
+from bottle import route, run, template, request, static_file  hook, response
 
 _quotes = {'timestamp': 'n/a', 'data': []}
 
@@ -35,15 +35,11 @@ def excel_dtm_to_str(xldate):
     except:
         return "n/a"
 
-# @hook('after_request')
-# def enable_cors():
-#     """
-#     You need to add some headers to each request.
-#     Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
-#     """
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST'
-#     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+@hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    # response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST'
+    # response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
         
 @route('/hello/<name>')
 def index(name):
