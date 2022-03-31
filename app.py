7d85293294f5864d7adf49cba50e4a858ff49fe7
Leonardo Horta <leonardo.horta@sparta.com.br>
2022-03-31 17:53:04 -0300

    caution lock

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from threading import Lock
 _quotes = {'timestamp': 'n/a', 'data': []}
 _lock = Lock()
 
+
 def excel_dtm_to_str(xldate):
     try:
         xldate = float(xldate)
@@ -37,15 +38,18 @@ def excel_dtm_to_str(xldate):
     except:
         return "n/a"
 
+
 @hook('after_request')
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
     # response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST'
     # response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
+
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
 
 @route('/feeder/<classe>', method='GET')
 def feeder(classe):
@@ -58,33 +62,36 @@ def feeder(classe):
     except:
         return template('<b>Last update: {{ts}}</b>', ts=_quotes['timestamp'])
 
+
 @route('/feeder')
 def feeder():
     return static_file('feeder.html', root='.')
     # global _quotes
     # return template('quotes.tpl', ts=_quotes['timestamp'], quotes=_quotes)
 
+
 @route('/getquotes')
 def get_quotes():
     return _quotes
 
+
 @route('/update', method='POST')
 def update():
-    global _quotes
+    global _lock, _quotes
     try:
+        _lock.acquire()
         _aux = {}
         # check for keys
-        if ('data' in request.json.keys() and
-            'header' in request.json.keys() and
-            'timestamp' in request.json.keys()):
+        if ('data' in request.json.keys() and 'header' in request.json.keys()
+                and 'timestamp' in request.json.keys()):
             _aux['timestamp'] = excel_dtm_to_str(request.json['timestamp'])
             _aux['header'] = request.json['header']
             _aux['data'] = request.json['data']
         # validade json
         print(_aux['data'][0])
-        if (len(_aux['header']) > 0 and
-            (len(_aux['header']) == len(_aux['data'][0])) and
-            _aux['timestamp'] != 'n/a'):
+        if (len(_aux['header']) > 0
+                and (len(_aux['header']) == len(_aux['data'][0]))
+                and _aux['timestamp'] != 'n/a'):
             # success
             _quotes = _aux
             print("Update: %s" % _quotes['timestamp'])
@@ -92,6 +99,8 @@ def update():
             print("invalid data")
     except:
         print("update failed")
+    finally:
+        _lock.release()
 
     return template('<b>Last update: {{ts}}</b>', ts=_quotes['timestamp'])
 
